e4ee61c157d99297569df9e593916a12c717fbf0
T-Gilbert <122938102+T-Gilbert@users.noreply.github.com>
2023-04-24 13:00:07 -0500

    Update main.cpp

--- Assignments/P04/main.cpp
+++ Assignments/P04/main.cpp
@@ -1,1 +1,34 @@
-Holder
+import random
+from typing import List
+
+from Dice import Die, Dice
+
+if __name__ == '__main__':
+
+# Testing the Die/Dice class
+# Produce random integer between 1 and 6
+  
+    d6 = Die()
+    print(d6.roll())  
+
+# Produce random integer between 1 and 6
+  
+    dice1 = Dice()
+    print(dice1.roll())
+
+# Produce a random integer between 2 and 20
+
+    dice2 = Dice(2, 10)
+    print(dice2.roll())  
+
+# Produce a random integer between 3 and 24 (3x8)
+    
+    dice3 = Dice("3.d.8")
+    print(dice3.roll())  
+    
+# The test of max roll and avg roll 
+  
+    dice4 = Dice(3, 6)
+    print(dice4.max_roll())  # should print 18 (3 * 6)
+    print(dice4.avg_roll())  # should print 4 (average of 3 and 3 and 3)
+    
